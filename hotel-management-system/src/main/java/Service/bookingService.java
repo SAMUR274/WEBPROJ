2b1b52b9e8a2d82c89be24f9b769e4f776476aa8
amandeep saroa <amandeep.saroa@ontariotechu.net>
2024-03-18 03:24:30 -0400

    Added Optional Lib to booking Service

--- hotel-management-system/src/main/java/Service/bookingService.java
+++ hotel-management-system/src/main/java/Service/bookingService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import Model.booking;
 import Repository.bookingRepository;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class bookingService {
